57ba015d34a226a279ad7b064709c54c3c92ce4f
saradeveloper92 <sara.elifani92@gmail.com>
2021-09-14 16:11:32 +0200

    versione con prompt

--- calcolatrice.js
+++ calcolatrice.js
@@ -1,33 +1,51 @@
 
-var a = 60;
-var b = 5;
-
-function main() {
-    console.log(divisione(a, b) + "\n" + somma(a, b) + "\n" + sottrazione(a, b) + "\n" + moltiplicazione(a, b));
-}
-
+let a = prompt("Scrivi il primo numero");
+let b = prompt("Scrivi il secondo numero");
+let operazione = prompt("Quale operazione vuoi esegiure? somma, sottrazione, moltiplicazione, divisione");
 
 function somma(a, b) {
-    return ("Il risultato della somma: ") + (a + b);
+    return ("Il risultato della somma: ") + (parseInt(a) + parseInt(b));
 }
 
 function sottrazione(a, b) {
-    return ("Il risultato della sottrazione: ") + (a - b);
+    return ("Il risultato della sottrazione: ") + (parseInt(a) - parseInt(b));
 }
 
 function moltiplicazione(a, b) {
-    return ("Il risultato della moltiplicazione: ") + (a * b);
+    return ("Il risultato della moltiplicazione: ") + (parseInt(a) * parseInt(b));
 }
 
 function divisione(a, b) {
-    if (b != 0) {
-        return ("Il risultato della divisione: ") + (a / b);
+    if (b == 0) {
+        console.log("Non e' possibile dividere per zero");
     } else {
-        console.log("Non e' possibile dividere per zero")
+        return ("Il risultato della divisione: ") + (parseInt(a) / parseInt(b));
     }
 }
 
-//per richiamare metodo main 
+switch (operazione) {
+    case "somma":
+        console.log(somma(a, b));
+        break;
+    case "sottrazione":
+        console.log(sottrazione(a, b));
+        break;
+    case "moltiplicazione":
+        console.log(moltiplicazione(a, b));
+        break;
+    case "divisione":
+        console.log(divisione(a, b));
+        break;
+}
+
+
+
+
+
+
+/*
+//per richiamare metodo main
 if (require.main === module) {
     main();
 }
+*/
